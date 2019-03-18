c0f9a9a91e2a28274794687e665a55a87c3c97ca
Andriy Gudz <infandrew@gmail.com>
2019-03-18 02:28:32 +0200

    Added init.R to create basic rda plot

--- dummy2.R
+++ dummy2.R
@@ -1,12 +1,15 @@
 require("vegan")
 
-#mod <- rda(fok20, scale=TRUE)
-#plot(mod, scaling = 3)
+data(dune)
+data(dune.env)
+#mod <- rda(dune ~ Management, data = dune.env)
+#plot(mod)
 
 data(dune, dune.env)
 mod <- rda(dune, scale=TRUE)
-
 with(dune.env, levels(Use))
+
+
 scl <- 3
 
 colvec <- c("red2", "green4", "mediumblue")
@@ -21,3 +24,7 @@ text(mod, display = "species", scaling = scl, cex = 0.8, col = "darkcyan")
 
 with(dune.env, legend("topright", legend = levels(Use), bty = "n",
                       col = colvec, pch = 21, pt.bg = colvec))
+
+
+#var.sc <- scores(mod, choices=1:2, scaling=3, display="sp")
+#arrows(0,0, var.sc[,1], var.sc[,2], length=0, lty=1, col="red")
